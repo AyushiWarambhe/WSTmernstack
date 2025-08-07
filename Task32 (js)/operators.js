7825fb69915428455504947eb42e69608ef54974
AyushiWarambhe <[warambheayushi@gmail.com]>
2025-08-07 23:52:15 +0530

    js operators

---
+++ Task32 (js)/operators.js
@@ -0,0 +1,21 @@
+console.log ("welcome in the world of javascript")
+
+
+//Arithmetic Operatos
+let a = 10;
+let b = 3;
+console.log(a + b);  // 13
+console.log(a - b);  // 7
+console.log(a * b);  // 30
+console.log(a / b);  // 3.333...
+console.log(a % b);  // 1
+console.log(a ** b); // 1000
+
+
+//Assignments Operators
+let x = 10;
+console.log(x += 5); //15   
+console.log(x -= 3); //12
+console.log(x *= 2);//24 
+console.log(x /= 4);//6  
+console.log(x %= 4); //2
