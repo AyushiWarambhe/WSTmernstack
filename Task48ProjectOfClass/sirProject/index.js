5e6a4986ca45fd1fcfc737973c4d25972f67a456
AyushiWarambhe <[warambheayushi@gmail.com]>
2025-09-10 22:20:59 +0530

    task button

---
+++ Task48ProjectOfClass/sirProject/index.js
@@ -0,0 +1,88 @@
+let tasks = []
+
+let add_notes_button = document.querySelector("#add-notes-button")
+
+let targetElement = document.querySelector(".add-notes-pop-up")
+
+let closeButton = document.querySelector('#close-pop-up')
+
+let taskForm = document.querySelector("#task-form")
+
+let addNotesFormContainer = document.querySelector(".add-notes-form")
+
+add_notes_button.addEventListener('click', (event) => {
+    targetElement.classList.add("active")
+})
+
+closeButton.addEventListener('click', () => {
+    targetElement.classList.remove("active")
+})
+
+document.querySelector("#task-form").onsubmit = (e) => {
+  e.preventDefault();
+  tasks.push({
+    title: taskTitle.value,
+    desc: taskDesc.value
+  });
+  document.querySelector(".add-notes-pop-up").style.display = "none";
+};
+
+addNotesFormContainer.addEventListener("mouseleave", () => {
+    document.getElementById("formSubmitButton").click()
+})
+
+
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    try {
+        if (!event.target["title"].value || !event.target["description"].value) {
+            throw ("empty fields !")
+        }
+        tasks.push({
+            title: event.target["title"].value,
+            description: event.target["description"].value,
+            timeStamp: `T: ${new Date().toLocaleTimeString()} D: ${new Date().toLocaleDateString()}`
+        })
+
+        event.target["title"].value = ""
+        event.target["description"].value = ""
+
+        closeButton.click()
+
+        displayTask()
+
+    } catch (err) {
+        console.log("please added task data before submitting ! : ", err)
+    }
+})
+
+function displayTask() {
+    document.querySelector('.tasks-container').innerHTML = ""
+    tasks.forEach((task, index) => {
+        let singleTask = document.createElement("div")
+        singleTask.classList.value = "task border p-4"
+        singleTask.innerHTML = `
+                            <h4 class="title">${task.title}</h4>
+                            <p class="description">
+                                ${task.description}
+                            </p>
+                            <span class="timeStamp">${task.timeStamp}</span>
+                            <button onClick='deleteTask(${index})'>delete</button>
+                            <button onClick='editTask(${index})'>Edit</button>
+    `
+        document.querySelector('.tasks-container').appendChild(singleTask)
+    })
+}
+function deleteTask(deleteIndex) {
+    tasks = tasks.filter((task, index) => { return index != deleteIndex })
+    displayTask()
+}
+
+function editTask(editIndex) {
+    // open pop up
+    targetElement.classList.add("active")
+    // access the data for selected task
+    console.log(tasks[editIndex].title)
+    console.log(tasks[editIndex].description)
+    // put selected data into input fields
+}
